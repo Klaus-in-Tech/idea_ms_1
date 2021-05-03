d4dedfa2de9382d3a79d2ec41b13e78037b3cc04
Allan Klaus KA. Kakooza <KakoozaA2@stanbic.com>
2021-05-03 16:44:17 +0300

    CRUD Frontend

--- idea_ms-main/src/main/java/com/flyhub/ideamanagementsystem/controller/IdeaController.java
+++ idea_ms-main/src/main/java/com/flyhub/ideamanagementsystem/controller/IdeaController.java
@@ -1,17 +1,12 @@
 package com.flyhub.ideamanagementsystem.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.flyhub.ideamanagementsystem.entity.Idea;
@@ -24,8 +19,9 @@ public class IdeaController {
 
 	@Autowired
 	private IdeaRepository ideaRepository;
+
 	@Autowired
-	//private IdeaService ideaService;
+	// private IdeaService ideaService;
 
 	// Register idea
 	@PostMapping("/register_idea")
@@ -33,25 +29,28 @@ public class IdeaController {
 		ideaRepository.save(idea);
 		return ("redirect:/idea_form");
 	}
+
 	@GetMapping("/idea_form")
 	public String showIdeaForm(Model model) {
 		model.addAttribute("idea", new Idea());
-		return("idea_form");
+		return ("idea_form");
 	}
 
 	// update idea
-	@PostMapping("/edit_idea/{id}")
-	public ResponseEntity<Idea> updateIdea(@PathVariable(value = "id") Long ideaId, Idea ideaDetails)
+	@GetMapping("/edit_idea/{id}")
+	public String showIdeaForm(@PathVariable(value = "id") Long ideaId, Model model)
 			throws ResourceNotFoundException {
 
 		Idea idea = ideaRepository.findById(ideaId)
 				.orElseThrow(() -> new ResourceNotFoundException("Idea not found for this id :: " + ideaId));
-
-		idea.setIdea_title(ideaDetails.getIdea_title());
-		idea.setDate_of_submission(ideaDetails.getDate_of_submission());
-		idea.setIdea_description(ideaDetails.getIdea_description());
-
-		return ResponseEntity.ok(this.ideaRepository.save(idea));
+		model.addAttribute("idea", idea);
+		return "edit_idea";
+	}
+	
+	@PostMapping("/update_idea/{id}")
+	public String updatedIdea(@PathVariable(value="id") Long ideaId, Idea ideaDetails) {
+		ideaRepository.save(ideaDetails);
+		return "redirect:/ideas";
 	}
 
 	// delete idea
@@ -66,7 +65,7 @@ public class IdeaController {
 		 * Map<String, Boolean> response = new HashMap<>(); response.put("deleted",
 		 * Boolean.TRUE);
 		 */
-		model.addAttribute("ideas",ideaRepository.findAll());
+		model.addAttribute("ideas", ideaRepository.findAll());
 		return "idea_list";
 
 	}
@@ -74,11 +73,10 @@ public class IdeaController {
 	// get all ideas
 	@GetMapping("/ideas")
 	public String getAllIdea(Model model) {
-		model.addAttribute("ideas",ideaRepository.findAll());
+		model.addAttribute("ideas", ideaRepository.findAll());
 		return "idea_list";
-		
+
 	}
-	
 
 	// get idea by id
 	@GetMapping("/idea/{id}")
